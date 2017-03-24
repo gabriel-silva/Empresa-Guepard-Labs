878e55dc67402099b5126013bdc7a002f163fded
odinAsgardian <odinmiguel97@gmail.com>
2017-03-24 09:16:23 -0300

    adicionando mapa & pharsal verbs

---
+++ SiteCelle/php/phrasal_verbs.php
@@ -0,0 +1,98 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<title>Phrasal Verbs</title>
+	</head>
+	<body>
+		<h1>OS PHRASAL VERBS MAIS USADOS EM INGLÊS</h1>
+		<p> Os phrasal verbs combinam um verbo com uma preposição, um verbo com um advérbio ou podem, ainda, ser tudo ao mesmo tempo agora: verbo, preposição e advérbio. Mas como assim, gente? 
+		</p>
+		<p> Esse tipo de verbo bagunça a cabeça de quem está estudando, sim senhor. Sabe por quê? Eles, muitas vezes, mudam totalmente o significado original do verbo.</p>
+		<p>Ou seja, se você disser “I will ask her out”, o verbo “ask” não será traduzido ao pé da letra, como “perguntar”. O correto é traduzir essa frase como “Vou convidá-la para sair”. Observe que no fundo, no fundo, o significado original até veio a calhar, né? Nem sempre é assim, contudo − mas dá para se acostumar.</p>
+		<p>Vamos desfazer algumas confusões mostrando a vocês alguns dos principais phrasal verbs e uma frase que os contextualiza:
+		</p>
+		
+		<p>
+			<b>Give up. “Don't give up university.”</b><br> 
+		Desistir. “Não desista da universidade.”</p>
+
+		<p>
+			<b>Go on. “We can't go on like this anymore.”</b><br> 
+		Continuar. "Não podemos mais continuar assim."</p>
+		<p>
+			<b>Get on well with. “I get on well with her sister. A lot.”</b><br> 
+		Dar-se bem com alguém. “Eu me dou muito bem com a irmã dela.”</p>
+		<p>
+			<b>Get home. “I feel so exhausted that I can't wait until I get home!”</b><br> 
+		Chegar em casa. “Estou tão exausto que mal posso esperar até chegar em casa!”
+
+		<p>	
+			<b>Get out! “You’ve done too much mess already. Now just get out of here!”</b><br> 
+		Vá embora! “Você já fez bagunça demais. Agora simplesmente vá embora!”</p>
+		<p>
+			<b>Get over / Get over with. “She has been working on that forever − I wonder if she will ever get it over with”.</b><br> 
+			Acabar. Terminar isso. “Ela tem trabalhado nisso há séculos − pergunto-me se ela, um dia, vai terminar.” </p>
+
+		<p>
+			<b>Run into. “Can you believe I just ran into him on the subway station?”</b><br> 
+			Esbarrar. “Dá para acreditar que esbarrei nele na estação de metrô?” </p>
+
+		<p>
+			<b>Pick up. “Yes, they will pick me up at home for us to start the road trip.”</b><br> 
+		Pegar. “Sim, eles virão me pegar em casa para começarmos nossa viagem.”</p>
+		<p>	
+			<b>Come back:</b> Voltar.
+		</p>
+		<p>
+			<b>Call back:</b>Retornar a ligação.
+		</p>
+		<p>
+			<b>Find out:</b> Descobrir.
+		</p>
+		<p>
+			<b>Set up:</b> Estabelecer, construir.
+		</p>
+		<p>
+			<b>Bring up:</b> Levantar (um tópico, por exemplo).
+		</p>
+		<p>
+			<b>Hang on:</b> Esperar um pouco.
+		</p>
+		<p>
+			<b>Run out of:</b>Ficar sem alguma coisa.
+		</p>
+		<p>
+			<b>Cheer up:</b> Animar.
+		</p>
+		<p>
+			<b>Drop off:</b> Deixar algo em algum lugar.
+		</p>
+		<p>
+			<b>Look over:</b> Revisar.
+		</p>
+		<p>
+			<b>Throw out ou Throw away:</b> Descartar, jogar fora.
+		</p>
+		<p>
+			<b>Think over:</b> Considerar.
+		</p>
+		<p>
+			<b>Put on:</b> Vestir.
+		</p>
+		<p>
+			<b>Pissed off:</b> Chatear/Irritar.
+		</p>
+		<p>
+			<b>Pull over:</b>Estacionar.
+		</p>
+		<p>
+			<b>Make out:</b>Escutar/Entender.
+		</p>
+		<p>
+			<b>Came out:</b> Sair. 
+		</p>
+		
+
+	</body>
+</html>
