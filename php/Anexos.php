651b20bd99d86fa04759a357054ef95bb9b6f156
Mr-Holmes <mugiwara.gl@gmail.com>
2017-03-30 14:11:09 -0300

    Create Anexos.php

---
+++ php/Anexos.php
@@ -0,0 +1,160 @@
+<?php 
+	include_once 'cabecalho.php';
+	?>
+	<html lang="pt-br">
+		<head>
+			<meta charset="utf-8">
+			<title>Imagens e Aúdios</title>
+		</head>
+		<body>
+
+			 <section>
+        <div id="conteudo">  
+            <h1 class="t1">Anexos</h1>
+            <hr id="hr-top"></hr>
+            <div class="Anexos">
+
+
+			<table>
+				<tr>
+					<td>
+						<figure>	
+							<img src="../Imagens/imagens_mafia/airplane.jpg">
+						</figure>
+						<audio controls>
+					  		<source src="../audios/audios_mafia/airplane.mp3" type="audio/mp3">
+					 
+						</audio>
+					</td>
+					<td>
+						<figure>	
+							<img src="../Imagens/imagens_mafia/american_football.jpg">
+						</figure>
+						<audio controls>
+					  		<source src="../audios/audios_mafia/american_football.mp3" type="audio/mp3">
+					 
+						</audio>
+					</td>
+					<td>
+						<figure>	
+							<img src="../Imagens/imagens_mafia/apple.jpg">
+						</figure>
+						<audio controls>
+					  		<source src="../audios/audios_mafia/apple.mp3" type="audio/mp3">
+					 
+						</audio>
+					</td>
+					<td>
+						<figure>	
+							<img src="../Imagens/imagens_mafia/archery.jpg">
+						</figure>
+						<audio controls>
+					  		<source src="../audios/audios_mafia/archery.mp3" type="audio/mp3">
+					 
+						</audio>
+					</td>
+				</tr>
+				<tr>
+					</audio>
+					</td>
+					<td> 
+						<figure>	
+							<img src="../Imagens/imagens_mafia/arm.jpg">
+						</figure>
+						<audio controls>
+					  		<source src="../audios/audios_mafia/arm.mp3" type="audio/mp3">
+					 
+						</audio>
+						<td>
+						<figure>	
+							<img src="../Imagens/imagens_mafia/auditorium.jpg">
+						</figure>
+						<audio controls>
+					  		<source src="../audios/audios_mafia/auditorium.mp3" type="audio/mp3">
+					 
+						</audio>
+
+					</td>
+					<td>
+						<figure>	
+							<img src="../Imagens/imagens_mafia/back.jpg">
+						</figure>
+						<audio controls>
+					  		<source src="../audios/audios_mafia/back.mp3" type="audio/mp3">
+					 
+						</audio>
+					</td>
+					<td>
+						<figure>	
+							<img src="../Imagens/imagens_mafia/baseball.jpg">
+						</figure>
+						<audio controls>
+					  		<source src="../audios/audios_mafia/baseball.mp3" type="audio/mp3">
+					 
+						</audio>
+					</td>
+
+				</tr>
+				<tr>
+					<td>
+						<figure>	
+							<img src="../Imagens/imagens_mafia/bear.jpg">
+						</figure>
+						<audio controls>
+					  		<source src="../audios/audios_mafia/bear.mp3" type="audio/mp3">
+					 
+						</audio>
+					</td>
+					<td>
+						<figure>	
+							<img src="../Imagens/imagens_mafia/belly.jpg">
+						</figure>
+						<audio controls>
+					  		<source src="../audios/audios_mafia/belly.mp3" type="audio/mp3">
+					 
+						</audio>
+					</td>
+					<td>
+						<figure>	
+							<img src="../Imagens/imagens_mafia/bicycle.jpg">
+						</figure>
+						<audio controls>
+					  		<source src="../audios/audios_mafia/bicycle.mp3" type="audio/mp3">
+					 
+						</audio>
+					</td>
+
+					<td>
+						<figure>	
+							<img src="../Imagens/imagens_mafia/bird.jpg">
+						</figure>
+						<audio controls>
+					  		<source src="../audios/audios_mafia/bird.mp3" type="audio/mp3">
+					 
+						</audio>
+					</td>
+
+					<tr>
+						<td>
+						<figure>	
+							<img src="../Imagens/imagens_mafia/blackboard.jpg">
+						</figure>
+						<audio controls>
+					  		<source src="../audios/audios_mafia/blackboard.mp3" type="audio/mp3">
+					 
+						</audio>
+					</td>
+					</tr>
+
+				</tr>
+			
+			       </div>
+            </div>  
+        </table>
+    </section>
+		</body>
+	</html>
+	<?php 
+	include_once 'rodape.php';
+?>
+
