97eac0d5ab9b592acb7144cdd444596ad376f695
suenesilva <suenesilvasantana@hotmail.com>
2017-03-30 16:05:27 -0300

    adicionando termos tecnicos de logistica

---
+++ php/termostecnicosdelogistica.php
@@ -0,0 +1,120 @@
+<?php
+include_once 'cabecalho.php';
+?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<title>Termos técnicos de logistica</title>
+	</head>
+	<body>
+	<section>
+        <div id="conteudo">  
+            <h1 class="t1">Termos técnicos de logistica</h1>
+            <hr id="hr-top"></hr>
+           <div class="expressoes">
+			
+				<table>
+				<tr>
+					<th>Termos</th>
+					<th>Significado</th>
+				</tr>
+					<tr>
+						<td> supply </td>
+						<td>Abastecimento - 
+                           é objeto de estudo da administração de materiais. Trata-se do suprimento, a um processo administrativo ou operacional, dos elementos necessários (recursos) a um órgão ou instituição para que possa atingir seus objetivos.</td>
+					</tr>
+
+					<tr>
+						<td>Access Point </td>
+						<td> ponto de acesso -
+                        Na logística, pode significar os pontos de acesso a informações relacionadas com etiquetagem de mercadorias nas docas de carga ou descarga ou em pontos estratégicos nos armazéns ou centros de distribuição.  </td>
+					</tr>
+
+					<tr>
+						<td>Supply Chain Management </td>
+						<td>Administração da Cadeia de Abastecimento –trata-se de uma abordagem que objetiva oferecer o máximo valor ao cliente e o máximo retorno sobre os ativos fixos, através de uma gestão efetiva dos fluxos materiais, produtos, informações e recursos financeiros, desde o fornecedor primário até o consumidor/usuário final da cadeia de abastecimento, de um extremo a outro da cadeia.
+</td>
+					</tr>
+					
+					<tr>
+						<td>Categories Administration </td>
+						<td>Administração de Categorias – Técnica de gestão de categorias de produtos como centros de resultados, de forma a garantir que todas as categorias de produtos contribuam com margens de lucros positivas. 
+ </td>
+					</tr>
+
+					<tr>
+						<td>Waterway </td> 
+						<td>Aquaviário - modo de transporte utilizando as vias aquáticas, ou seja, abrangendo o transporte marítimo, fluvial e lacustre. 
+</td>
+					</tr>
+
+					<tr>
+						<td>Broker</td> 
+						<td>Corretor -
+                      Na acepção mais pura, um broker é uma empresa prestadora de serviços de vendas, promoção, merchandising, pesquisa de mercado e ações no ponto-de-venda, como degustação e abordagem, sem comprar ou vender nada, trabalhando para um número limitado de empresas e nunca para mais de uma num mesmo segmento. </td>
+					</tr>
+
+					<tr>
+						<td>Break-Bulk </td> 
+						<td>Carga fracionada - expressão utilizada no transporte marítimo que significa o transporte de carga geral. 
+
+</td>
+					</tr>
+
+
+					<tr>
+						<td>Bucket</td>
+						<td>Balde -
+                    trata-se da unidade principal de medição de tempo nos sistemas de planejamento das necessidades de materiais. Usualmente, utiliza-se uma semana igual a um bucket. 
+					</tr>
+
+					<tr>
+						<td>Supply chain  </td> 
+						<td> Cadeia de mantimentos-é constituída pelo conjunto de organizações que se inter-relacionam, agregando valor na forma de produtos e serviços, desde os fornecedores de matéria-prima até o consumidor/usuário final. 
+
+</td>
+					</tr>
+
+					<tr>
+						<td>Distribution channel </td>
+						<td>Canal de Distribuição -
+                      caminho percorrido pelos produtos/serviços de uma empresa até chegarem aos seus clientes/usuários finais, ou o roteiro pelo qual uma empresa distribui seus produtos/serviços. 
+
+					</tr>
+
+					<tr>
+						<td>Storage Capacity</td> 
+						<td> Capacidade de Armazenagem – 
+                     quantidade de materiais (matérias-primas, insumos, produtos acabados, embalagens etc.) que um armazém consegue abrigar em determinado espaço físico disponível. 
+</td>
+					</tr>
+						<tr>
+						<td>Production capacity </td>
+						<td>Capacidade de Produção – é a taxa de produção máxima de uma organização. Também chamada de capacidade efetiva. 
+
+					</tr>
+
+					<tr>
+						<td>Process Capability </td> 
+						<td> Capacidade de Processo – é a capacidade do processo produtivo para fabricar produtos dentro das expectativas dos clientes. 
+</td>
+					</tr>
+						<tr>
+						<td>Export </td> 
+						<td> Exportação – é a venda de produtos e/ou serviços para um comprador localizado em outro país. 
+
+</td>
+					</tr>
+
+
+				</div>
+            	</div>  
+			</table>
+    </section>
+
+	</body>
+</html>
+<?php 
+include_once 'rodape.php';
+ ?>
