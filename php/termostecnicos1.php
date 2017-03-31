5d330b4c18c0d02617d0253d4faee1d17724f09f
Mr-Holmes <mugiwara.gl@gmail.com>
2017-03-31 00:41:30 -0300

    Update termostecnicos1.php

--- php/termostecnicos1.php
+++ php/termostecnicos1.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 include_once 'cabecalho.php';
 ?>
 <html lang="pt-br">
@@ -11,83 +11,177 @@ include_once 'cabecalho.php';
         <div id="conteudo">
             <h1 class="t1"> Termos Técnicos </h1>
             <hr id="hr-top"></hr>
-            <div class="paises">
+            <div class="termo">
                 <table>
                     <tr>
                         <th>Termo</th>
                         <th>Significado</th>
-                        
+
                     </tr>
+
                     <tr>
-                        <td>JAVA</td>
-                        <td>Java é uma linguagem de programação interpretada orientada a objetos. Diferente das linguagens de programação convencionais, que são compiladas para código nativo, é compilada para um bytecode (código de um programa escrito na linguagem Java) que é executado por uma máquina virtual (JVMs)</td>
-                        
+
+                         <td>Memória RAM</td>
+                         <td>(Random Access Memory) Memória de Acesso Randômico. Memória composta de diversos chips nos quais o computador tanto armazena quanto lê dados temporariamente. A memória RAM é volátil, ou seja, se faltar energia você perderá os dados nela armazenados que estão sendo processados no momento.</td>
+
                     </tr>
+
                     <tr>
-                        <td>LAN</td>
-                        <td>A LAN (local area network – rede de área local) é uma rede local que tem por finalidade a troca de dados dentro um mesmo espaço físico. O limitador da rede LAN é uma faixa de IP restrita à mesma, com uma máscara de rede comum.
-                       </td>
-                        
+
+                         <td>  MPEG </td>
+                         <td>(Moving Pictures Expert Group). Tipo de arquivo utilizado para armazenar animações.</td>
+
                     </tr>
+
                     <tr>
-                        <td> Mainframe</td>
-                        <td>É um computador de grande porte, com foco no processamento de um grande volume de informações.
-                        </td>
-                        
-                    </tr>   
-                    <tr>
-                        <td>MySQL</td>
-                        <td>O MySQL é um SGBD (sistema de gestão de banco de dados) que utiliza a linguagem SQL como interface.
- 
-                        </td>
-                        
-                    </tr>   
-                    <tr>
-                        <td>Netflow</td>
-                        <td>A partir de sua instalação no roteador Cisco, o NetFlow passa a identificar os pacotes de dados não mais isoladamente, como outras tecnologias, mas como fluxos com início, meio e fim. Quando os fluxos são identificados, eles são armazenados no NetFlow Cache para caracterização e compreensão do tráfego da rede. Após 30 minutos são apagados da memória.
-                        </td>
-                        
-                    </tr>   
-                    <tr>
-                        <td> SQL</td>
-                        <td>SQL (structured query language – linguagem de consulta estruturada) é a linguagem de pesquisa declarativa padrão para banco de dados relacional.
-                        </td>
-                       
-                    </tr>   
-                    <tr>
-                        <td> TCP/IP</td>
-                        <td>O TCP/IP é um conjunto de protocolos de comunicação entre computadores em rede. Seu nome vem de dois protocolos: o TCP (transmission control protocol – protocolo de controle de transmissão) e o IP (internet protocol – protocolo de internet). O conjunto de protocolos pode ser visto como um modelo de camadas, onde cada camada é responsável por um grupo de tarefas, fornecendo um conjunto de serviços bem definidos para o protocolo da camada superior.
-                        </td>
-                       
-                    </tr>   
-                    <tr>
-                        <td>URL</td>
-                        <td>A URL (uniform resource locator – localizador padrão de recursos) refere-se ao endereço de rede no qual se encontra algum recurso informático, como por exemplo um arquivo de computador ou um dispositivo periférico.
-                        </td>
-                        
-                    </tr>   
-                    <tr>
-                        <td> VPN</td>
-                        <td>VPN (virtual private network – rede privada virtual) é uma rede de comunicação privada. Por fornecerem autenticação e confidencialidade na transmissão de dados, além de protocolos criptografados por tunelamento, as VPNs tornam-se mais seguras e confiáveis nas comunicações.
-                        </td>
-                       
-                    </tr>   
-                    <tr>
-                        <td> WEB</td>
-                        <td>WWW (world wide web) ou WEB é a sigla que denomina a rede mundial de computadores ligados em rede.
-                        </td>
-                       
+
+                         <td> MS-DOS</td>
+                         <td>(Microsoft-Disk Operation System) Sistema Operacional em Disco da Microsoft. É a galinha dos ovos de ouro da Microsoft, pois um computador sem DOS não funciona.</td>
+
+                    </tr>
+
                     
+                    <tr>
+
+                         <td> PC </td>
+                         <td>(Personal Computer – computador pessoal). Primeiro computador pessoal, de onde surgiu o nome. Termo que designa computadores que utilisam processador da família Intel e compatíveis. </td>
+
+                    </tr>
+
+                    <tr>
+
+                         <td>Packet Radio</td>
+                         <td>(Rádio Pacote). Sistema de comunicação à distância usando um computador conectado a um aparelho de radioamador.</td>
+
+                    </tr>
+
+                     <tr>
+
+                         <td>PCI</td>
+                         <td>(Peripheral Component Interconnect). Interconexão de componentes periféricos. Um padrão de barramento local inventado pela Intel que permite a adição de até 10 dispositivos de barramento local e suporta operação simultânea da CPU e do barramento mestre. </td>
+
+                    </tr>
+
+                     <tr>
+
+                         <td>PDF </td>
+                         <td>(Portable Document Format). Formato de arquivo criado pela Adobe. O PDF permite o envio de documentos formatados para que sejam vistos ou impressos em outro lugar, sem a presença do programa que o gerou. </td>
+
+                    </tr>
+
+                    <tr>
+
+                         <td>PIF </td>
+                         <td>(Program Information File). Arquivo que contém informações sobre que medidas o Windows 3.x deverá tomar quando for executar um programa desenvolvido para rodar sob DOS. </td>
+
+                    </tr>
+
+                    <tr>
+
+                         <td>PIXELS </td>
+                         <td>(Pixel Picture Element). É o menor ponto de luz cuja cor e luminosidade podem ser controladas na tela. As imagens são formadas com a combinação de grande número de pixels. </td>
+
+                    </tr>
+
+                    <tr>
+
+                         <td>PPP</td>
+                         <td>(Point to Point Protocol). Protocolo de comunicação entre computadores por linha telefônica usado para conexão internet.</td>
+
+                    </tr>
+
+                    <tr>
+
+                         <td>Raiz</td>
+                         <td> (Root) Designa o diretório de onde derivam todos os outros. O mais alto na hierarquia. O termo root também se refere a superusuário. </td>
+
+                    </tr>
+
+                    <tr>
+
+                         <td>RAM </td>
+                         <td>(Random Access Memory).Memória cujas informações armazenadas podem ser alteradaas pelo usuário. As informações existentes na RAM não são estáveis e, caso não sejam salvas no disco, serão perdidas ao se desligar o computador. </td>
+
+                    </tr>
+
+                    <tr>
+
+                         <td> RTF </td>
+                         <td>(Rich Text Format). Formato de documentos de texto suportado por vários editores de texto. </td>
+
+                    </tr>
+
+                    <tr>
+
+                         <td> SGML</td>
+                         <td>(Standard Generalized Markup Language). Metalinguagem que define todos os tipos de linguagens de marcação, inclusive a linguagem HTML.</td>
+
+                    </tr>
+
+                     <tr>
+
+                         <td>SIMM </td>
+                         <td>(Single Inline Memory Module). Tipo de módulo de memória mais comum nos PCs. É uma pequena placa de circuitos impresso contendo vários chips de memória.</td>
+
+                     </tr>
+
+                    <tr>
+
+                         <td> SQL</td>
+                         <td> (structured query language – linguagem de consulta estruturada) é a linguagem de pesquisa declarativa padrão para banco de dados relacional. </td>
+
+                    </tr>
+
+                    <tr>
+
+                         <td> TCP/IP</td>
+                         <td> TCP (transmission control protocol – protocolo de controle de transmissão) e o IP (internet protocol – protocolo de internet). O TCP/IP é um conjunto de protocolos de comunicação entre computadores em rede. Seu nome vem de dois protocolos. </td>
+
+                    </tr>
+
+                   <tr>
+
+                         <td>TIF - TIFF</td>
+                         <td>(Tagged Image File Format). Extensão do conhecido formato de arquivo gráfico.</td>
+
+                   </tr>
+
+                    <tr>
+                         <td>URL</td>
+                         <td>(Uniform resource locator – localizador padrão de recursos) refere-se ao endereço de rede no qual se encontra algum recurso informático, como por exemplo um arquivo de computador ou um dispositivo periférico. </td>
+
+                    </tr>
+
+                    <tr>
+                         <td> VPN</td>
+                         <td>(Virtual private network – rede privada virtual) é uma rede de comunicação privada. Por fornecerem autenticação e confidencialidade na transmissão de dados, além de protocolos criptografados por tunelamento, as VPNs tornam-se mais seguras e confiáveis nas comunicações. </td>
+
+                    </tr>
+
+                    <tr>
+
+                         <td>USB </td>
+                         <td>(Universal Serial Bus). Nova interface para conexão ao micro, com funcionamento Plug and Plaay, capaz de receber de maneira simples e rápida até 127 dispositivos externos, ligados por meio de um computador. </td>
+
+                    </tr>
+
+                    <tr>
+                         <td> WWW</td>
+                         <td>(world wide web) ou WEB é a sigla que denomina a rede mundial de computadores ligados em rede. </td>
+
+                    </tr>
+
+
                 </table>
                 <br/>
                 <span><small>Page 1 of 2 </small></span>
-                <a class="next_prev" href=""></a>
+                <a class="next_prev" href="termostecnicos.php">Anterior</a>
                 <br/>
             </div>
         </div>
     </section>
 </body>
 </html>
-<?php 
+<?php
 include_once 'rodape.php';
 ?>
