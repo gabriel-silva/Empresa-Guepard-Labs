aa0c671a0248da2d95894bf7b2b59d266a6c5cf9
Mr-Holmes <mugiwara.gl@gmail.com>
2017-03-31 00:40:22 -0300

    Update termostecnicos.php

--- php/termostecnicos.php
+++ php/termostecnicos.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 include_once 'cabecalho.php';
 ?>
 <html lang="pt-br">
@@ -11,133 +11,273 @@ include_once 'cabecalho.php';
         <div id="conteudo">
             <h1 class="t1"> Termos Técnicos </h1>
             <hr id="hr-top"></hr>
-            <div class="paises">
+            <div class="termo">
                 <table>
                     <tr>
                         <th>Termo</th>
                         <th>Significado</th>
-                        
+
                     </tr>
                     <tr>
-                        <td>Arpanet</td>
-                        <td>A Arpanet (advanced research projects agency network) é a primeira rede de computadores à base de comutação de pacotes. Criada em 1969 pelo departamento de defesa dos EUA para interligar bases militares e departamentos de pesquisa do governo, deu origem à internet.</td>
-                        
+
+                          <td>Arpanet</td>
+                          <td>(Advanced research projects agency network) é a primeira rede de computadores à base de comutação de pacotes. Criada em 1969 pelo   departamento de defesa dos EUA para interligar bases militares e departamentos de pesquisa do governo, deu origem à internet.</td>
+
                     </tr>
+
+                    
                     <tr>
-                        <td>Array</td>
-                        <td>O array é um grupo de elementos com atributos semelhantes (como memória ou disco) que podem ser endereçados individualmente.
-                       </td>
-                        
+
+                         <td> BASIC </td>
+              		       <td>(Beginners All-purpose Symbolic Instruction Code). Linguagem de programação inventada por John Kemeny, do Dartmouth College, no final dos anos 60.</td>
+
                     </tr>
+
                     <tr>
-                        <td> Backup </td>
-                        <td>O backup é a cópia de dados de um dispositivo de armazenamento a outro para que possam ser restaurados em caso da perda dos dados originais, o que pode envolver apagamentos acidentais ou corrupção de dados.
-                        </td>
-                        
-                    </tr>   
+
+                        <td> BBS </td>
+                        <td>(Bulletin Board System)Ambiente virtual formado por computadores, modems e um software de comunicação ligados a uma ou mais linhas telefônicas. Os usuários ligam para o BBS, onde enviam e recebem mensagens e copiam programas, imagens, etc. Foi muito usado antes do aparecimento da Web.</td>
+
+                    </tr>
+
                     <tr>
+
+                        <td>BDE </td>
+                        <td>(Borland Database Engine), Mecanismo de Acesso a Banco de Dados desenvolvido pela empresa Borland, amplamente testado por ser usado no mundo todo.</td>
+
+                    </tr>
+
+					         <tr>
+
                         <td>Bios</td>
-                        <td>O bios (basic input output system) é o sistema básico de entrada e saída.
-                        </td>
-                        
-                    </tr>   
+                        <td> (Basic input output system) é o sistema básico de entrada e saída. </td>
+
+                   </tr>
+
+  					       <tr>
+
+  					           <td>BPS</td>
+					             <td>(Bits Per Second) .É uma medida de velocidade de transmissão de dados.</td>
+
+					         </tr>
+
+ 					         <tr>
+
+                       <td>CD-ROM</td>
+                       <td>(Compact Disk Read-Only Memory). Memória de Massa que permite apenas leitura de dados. Se o CD for W/R significa que pode ser gravado e lido.</td>
+
+                   </tr>
+
                     <tr>
-                        <td>Cache</td>
-                        <td>O cache é um dispositivo de acesso rápido, interno a um sistema, que serve de intermediário entre um operador de um processo e o dispositivo de armazenamento ao qual esse operador acede.
-                        </td>
-                        
-                    </tr>   
-                    <tr>
-                        <td>Cookies</td>
-                        <td>Cookies são pequenos arquivos que ficam armazenados no computador. Eles são feitos para guardar dados específicos de um cliente ou website, para serem acessados futuramente pelo servidor web de maneira mais rápida.
-                        </td>
-                       
-                    </tr>   
-                    <tr>
+
                         <td>CPU</td>
-                        <td>Central processor unit ou unidade central de processamento é o responsável pelo controle e execução das tarefas do computador, ou seja, é onde todas as operações do computador são processadas.
-                        </td>
-                       
-                    </tr>   
+                        <td>(Central processor unit) ou unidade central de processamento é o responsável pelo controle e execução das tarefas do computador, ou seja, é onde todas as operações do computador são processadas. </td>
+
+                   </tr>
+
+                   <tr>
+
+                       <td>CERN </td>
+                       <td> (Centre European Researche Nucleare) Centro europeu de pesquisa onde Tim Berners-Lee desenvolveu primeiramente a linguagem HTML em 1990.</td>
+
+                  </tr>
+
+				         	<tr>
+
+					            <td>CGI</td>
+					            <td> (Common Gateway Interface).Técnica de passagem e chamada de parâmetros que permite aos internautas transferirem dados entre servidores Web, desde que usem programas conforme especificações CGI. </td>
+
+					       </tr>
+
+                  <tr>
+
+                      <td>CGI </td>
+					            <td>(Common Gateway Interface).Técnica de passagem e chamada de parâmetros que permite aos internautas transferirem dados entre servidores Web, desde que usem programas conforme especificações CGI.</td>
+
+					       </tr>
+
+				      	 <tr>
+
+					            <td>COBOL</td>
+					            <td>(Common Business Oriented Language).Linguagem de programação muito antiga, criada pela Almirante Grace Hopper.</td>
+
+					       </tr>
+
                     <tr>
-                        <td>Datacenter</td>
-                        <td>O datacenter (DC) ou CPD (centro de processamento de dados) é um ambiente projetado para abrigar servidores e outros componentes como sistemas de armazenamento de dados e ativos de rede.
-                        </td>
-                        
-                    </tr>   
-                    <tr>
-                        <td>DBMS ou SGBD</td>
-                        <td>DBMS é a sigla para data base management system, em português a sigla é SGBD (sistema de gerenciamento de banco de dados).
-                        </td>
-                       
-                    </tr>   
-                    <tr>
-                        <td>DNS</td>
-                        <td>(domain name system – sitema de nomes e domínios) é um sistema que gerencia nomes e reconhece seus números de IP para fazer uma conexão. Devido ao DNS pode-se, por exemplo, digitar na barra de endereços do navegador ’www.ifpe.edu.br’ para acessar o site ao invés de um monte de números e pontos.
-                        </td>
-                       
-                    </tr>               
-                    <tr>
-                        <td>Ethernet</td>
-                        <td>É um protocolo de conexão para redes locais (LAN) com base no envio de pacotes.
-                        </td>
-                        
-                    </tr>   
-                    <tr>
-                        <td>Firewall</td>
-                        <td>O firewall é um dispositivo de uma rede de computadores que tem por objetivo aplicar uma política de segurança a um determinado ponto da rede.
-                        </td>
-                       
-                    </tr>   
-                    <tr>
-                        <td>FTP</td>
-                        <td>O FTP (file transfer protocol – protocolo de transferência de arquivos) é uma forma rápida e simples de transferência de arquivos na internet. Pode representar tanto o computador que transfere o arquivo quanto o servidor FTP.
-                        </td>
-                        
-                    </tr>   
-                    <tr>
-                        <td>Gateway</td>
-                        <td>Computador que interliga duas ou mais redes que usem protocolos de comunicação internos diferentes.
-                        </td>
-                       
-                    </tr>   
-                    <tr>
-                        <td>Hotspot</td>
-                        <td>Hotspot é o nome dado ao local onde a tecnologia wi-fi está disponível. São encontrados geralmente em locais públicos como cafés, restaurantes, hotéis e aeroportos onde é possível conectar-se à internet utilizando qualquer dispositivo que esteja preparado para se comunicar em uma rede sem fio do tipo wi-fi.
-                        </td>
-                       
-                    </tr>   
-                    <tr>
-                        <td>HMTL</td>
-                        <td>O HMTL (hyper text markup language) é a linguagem de marcação mais popular na internet.
-                        </td>
-                       
-                    </tr>   
-                    <tr>
-                        <td>HTTP</td>
-                        <td>O HTTP (hypertext transfer protocol – protocolo de transferência de hipertexto) é um protocolo de comunicação que decide como dois computadores ou programas transmitirão dados na Internet. Normalmente, o HTTP utiliza a porta 80 se comunicando na linguagem HTML.
-                        </td>
-                      
-                    </tr>   
-                    <tr>
-                        <td>HTTPS</td>
-                        <td>O HTTPS (hypertext transfer protocol secure – protocolo seguro de transferência de hipertexto ) é uma implementação do protocolo HTTP sobre uma camada adicional de segurança que utiliza o protocolo SSL/TLS (protocolos de segurança que protegem as telecomunicações via internet para serviços de transferência de dados.). Essa camada adicional permite que os dados sejam transmitidos por meio de uma conexão criptografada e que se verifique a autenticidade do servidor e do cliente por meio de certificados digitais.
-                        </td>
-                       
-                    </tr>   
-                    <tr>
-                        <td>IP</td>
-                        <td>O IP (internet protocol – protocolo de internet) é um endereço único que diferencia cada dispositivo conectado à internet.
-                        </td>
-                       
-                    </tr>   
-                    <tr>
-                        <td>IPv6</td>
-                        <td>O IPv6 é a versão mais atualizada do IP, e possui mais números e protocolos. Surgiu para substituir gradativamente o IPv4, que já não suporta mais o cenário atual de IPs.
-                        </td>
-                       
+
+                       <td>Datacenter</td>
+                      <td>(DC) ou (CPD) centro de processamento de dados é um ambiente projetado para abrigar servidores e outros componentes como sistemas de armazenamento de dados e ativos de rede. </td>
+
                     </tr>
-                </table>
+
+                    <tr>
+
+                      <td>DBMS ou SGBD</td>
+                      <td>(Data base management system), em português a sigla é SGBD (sistema de gerenciamento de banco de dados). </td>
+
+                    </tr>
+
+                    <tr>
+
+                       <td>DNS</td>
+                       <td>(Domain name system – sitema de nomes e domínios) é um sistema que gerencia nomes e reconhece seus números de IP para fazer uma conexão.</td>
+
+                </tr>
+
+				      	<tr>
+
+				             <td>DOS</td>
+				            	<td>(Disk Operation System) Sistema Operacional em Disco. Um computador sem DOS não funciona. Um sistema operacional é um conjunto de programas gravados em disco que permite e facilita a comunicação entre a máquina e o ser humano. </td>
+
+                </tr>
+
+					      <tr>
+
+					           <td>Dpi</td>
+				            	<td> (Dots per inch ou ponto por polegada).Medida da resolução da imagem de uma impressora ou monitor.</td>
+
+					     </tr>
+
+					      <tr>
+
+					           <td>DRAM </td>
+				             <td>(Dynamic Random Access Memory ou Dynamic RAM) Memória Dinâmica de Acesso Aleatório. Tipo de chip de memória que requer que seu conteúdo seja atualizado freqüentemente. </td>
+
+				      	</tr>
+
+					      <tr>
+
+					           <td>DTD</td>
+					           <td>(Document Type Definition) Definição de Tipo de documento. Especificação SGML que define os elementos de marcação, seus modelos de conteúdo e outros aspectos das linguagens de marcação.</td>
+
+					     </tr>
+
+					      <tr>
+
+					           <td>DVD</td>
+					           <td> (Digital Versatile Disk).Dispositivo para Disco de Vídeo, DVD é um disco com capacidade de gravar dados em um formato digital, é uma versão mais atual em relação ao CD, pois possui uma capacidade muito maior de armazenamento de dados e arquivos. </td>
+
+					     </tr>
+
+					      <tr>
+
+				           	<td>EDO RAM </td>
+					           <td>(Extended Data Out Random Access Memory). Tipo aperfeiçoado de memória RAM que se alega proporcionar aumento de desempenho de até 30%, com pouco custo adicional, aumentando a velocidade de transações de memória por eliminar estados de espera entre as execuções de comandos de leitura seqüencial.</td>
+
+					     </tr>
+
+					      <tr>
+
+					           <td>Emoticons </td>
+					           <td>(Emotional Icons – Ícones Emocionais). Combinação de caracteres que simbolizam sentimentos. São utilizados em mensagens trocadas na rede.</td>
+
+					     </tr>
+
+					      <tr>
+
+					           <td>FAQ</td>
+				             <td>(Frequently Asked Questions).Lista de perguntas e respostas mais freqüentes feitas pelos internautas.</td>
+
+					      </tr>
+
+
+                <tr>
+
+                     <td>FAT </td>
+                     <td>(File Allocation Table).Tabela de Alocação de Arquivos. Uma tabela, gravada no próprio disco, que indica quais setores estão disponíveis e quais contém dados de arquivos. Inicialmente, quando um disco é formatado, todos os setores estão disponíveis. </td>
+
+					     </tr>
+
+                <tr>
+
+                     <td>FTP</td>
+                     <td>(file transfer protocol – protocolo de transferência de arquivos) é uma forma rápida e simples de transferência de arquivos na internet. Pode representar tanto o computador que transfere o arquivo quanto o servidor FTP. </td>
+
+               </tr>
+
+					      <tr>
+
+                     <td>GIF</td>
+                     <td>(Graphics Interchange Format).Tipo de arquivo para armazenamento de imagens, desenvolvido pela CompuServe em 87 e amplamente difundido na Internet. Esses arquivos são identificados pelo sufixo .gif. </td>
+
+					     </tr>
+
+
+					      <tr>
+
+					           <td>GUI</td>
+					           <td>(Interface Gráfica ao Usuário). Interface que une ícones e funções para realizar tarefas e facilitar a vida do usuário.</td>
+
+					     </tr>
+
+					      <tr>
+
+                     <td> HD </td>
+                     <td> (Hard Disk – Disco Rígido). Disco Rígido é um sistema de armazenamento de alta capacidade, que por não ser volátil, é destinado ao armazenamento de arquivos e programas. </td>
+
+               </tr>
+
+                <tr>
+
+                     <td>HMTL</td>
+                     <td>(hyper text markup language) é a linguagem de marcação mais popular na internet. </td>
+
+               </tr>
+
+                <tr>
+
+                     <td>HTTP</td>
+                     <td>(hypertext transfer protocol – protocolo de transferência de hipertexto) é um protocolo de comunicação que decide como dois computadores ou programas transmitirão dados na Internet. </td>
+
+               </tr>
+
+                <tr>
+
+                     <td>HTTPS</td>
+                     <td>(hypertext transfer protocol secure – protocolo seguro de transferência de hipertexto ) é uma implementação do protocolo HTTP sobre uma camada adicional de segurança que utiliza o protocolo SSL/TLS (protocolos de segurança que protegem as telecomunicações via internet para serviços de transferência de dados.). </td>
+
+                </tr>
+
+					 
+
+					      <tr>
+
+                     <td>IP</td>
+                     <td>(internet protocol – protocolo de internet) é um endereço único que diferencia cada dispositivo conectado à internet. </td>
+
+               </tr>
+
+				      	<tr>
+
+					           <td>IRC</td>
+					           <td>(Internet Relay Chat). Sistema de “bate-papo” online que permite a vários pessoas “conversarem” ao mesmo tempo via internet.</td>
+
+					      </tr>
+
+				         <tr>
+
+					           <td>IRQ</td>
+					           <td>(Interrupt Rerquest). Um pedido de atenção e de serviço feito à CPU. Em termos técnicos, designa linhas utilizadas pelo hardware para notificar a CPU sobre a necessidade de tempo de processamento. </td>
+
+					      </tr>
+
+					       <tr>
+
+					           <td>JPEG /JPG</td>
+				             <td> (Joint Photographic Experts Group).Algoritmo para comprimir imagens. Arquivos deste tipo costumam ser menores que os arquivos tipo GIF e possuem alto grau de fidelidade, permitindo armazenar imagens de até 16,7 milhões de cores. </td>
+
+				       	</tr>
+
+                 <tr>
+
+                     <td>LAN</td>
+                     <td>(Local area network – rede de área local) é uma rede local que tem por finalidade a troca de dados dentro um mesmo espaço físico. O limitador da rede LAN é uma faixa de IP restrita à mesma, com uma máscara de rede comum. </td>
+
+
+                 </tr>
+
+
+				</table>
                 <br/>
                 <span><small>Page 1 of 2 </small></span>
                 <a class="next_prev" href="termostecnicos1.php">Próximo</a>
@@ -147,6 +287,6 @@ include_once 'cabecalho.php';
     </section>
 </body>
 </html>
-<?php 
+<?php
 include_once 'rodape.php';
 ?>
